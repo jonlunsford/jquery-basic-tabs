13d09460e357b22a23f8e8558591685c52691409
jonathon.lunsford <jonathon.lunsford@mindbodyonline.com>
2013-02-12 14:27:22 -0800

    added basic example

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 $(function() {
   
-  $('.tabbable').basicTabs({
-    customActiveClass: 'active',
+  $('.vertical-tabs').basicTabs({
     tabsParentSelector: '.nav-tabs',
     tabSelector: '.nav-tabs li',
     tabsContentSelector: '.tab-pane',
     verticalTabs: true
+  });
+
+  $('.horizontal-tabs').basicTabs({
+    tabsParentSelector: '.nav-tabs',
+    tabSelector: '.nav-tabs li',
+    tabsContentSelector: '.tab-pane'
   });
 
 }); // end doc.ready
